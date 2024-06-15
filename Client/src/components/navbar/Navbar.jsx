cc57338aeb947f80d829a7e171160d1e98854d5e
umerxgit2004 <umer.workplace174@gmail.com>
2024-06-15 23:35:58 +0500

    nabar with the toggle and responsvie ui

--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
 import "./navbar.scss"
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
   return (
     <nav className="">
       <div className="left">
-        <a href="/">
+        <a href="/" className="logo">
           <img src ="/logo.png"/>
           <span>UmerEstate</span>
         </a>
@@ -15,7 +18,18 @@ const Navbar = () => {
       </div>
       <div className="right">
       <a href="/">Sign in</a>
-      <a href="/">Sign up</a>
+      <a href="/" className="register">Sign up</a>
+      <div className="menuIcon">
+        <img src="/menu.png" alt="" onClick={() => setOpen(!open)}/>
+      </div>
+      <div className={open ? "menu active" : "menu"}>
+      <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+        <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+      </div>
       </div>
     </nav>
   )
